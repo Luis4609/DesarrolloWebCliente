798c6b914d3b58e6e08249b0ada0e6fc4a02dd31
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2021-10-20 20:22:01 +0200

    new commit:tabla

--- tablaValores/tabla.js
+++ tablaValores/tabla.js
@@ -1,9 +1,12 @@
 let mountains = [
-  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
-  { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
-  { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
-  { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
-  { name: "Monte Amiata", height: 1738, place: "Siena" },
+  { x: "Monte Falco", cosx
+  
+  
+  : 1658, place: "Parco Foreste Casentinesi" },
+  { x: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+  { x: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+  { x: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+  { x: "Monte Amiata", height: 1738, place: "Siena" },
 ];
 
 function generateTableHead(table, data) {
