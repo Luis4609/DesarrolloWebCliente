44039f91650cabe70c5335d952db2b1bd52ff554
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2021-10-20 17:23:07 +0200

    commit: canvas

---
+++ Canvas Examples/Graphs/main.js
@@ -0,0 +1,17 @@
+var myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 300;
+myCanvas.height = 300;
+
+var ctx = myCanvas.getContext("2d");
+
+function drawLine(ctx, startX, startY, endX, endY) {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+}
+function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle) {
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+  ctx.stroke();
+}
