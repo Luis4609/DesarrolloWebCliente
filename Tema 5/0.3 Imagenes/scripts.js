47358635493561753d8af844cf7378fea9678097
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-01-24 16:59:24 +0100

    API pexels and API imagenes generadas de caras

---
+++ Tema 5/0.3 Imagenes/scripts.js
@@ -0,0 +1,39 @@
+const request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "https://api.generated.photos/api/v1/faces?api_key=hTpFjVfgA-mt6-eXPD0FIg",
+  true
+); 
+
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    const data = JSON.parse(this.response);
+
+    const contenedor = document.getElementById("contenedor");
+    contenedor.setAttribute("class", "card-columns");
+
+    data.forEach((datos) => {
+      // Creamos una tarjeta
+      const tarjeta = document.createElement("div");
+      tarjeta.setAttribute("class", "card");
+      tarjeta.setAttribute("id", datos.id);
+
+      const datosApi = document.createElement("img");
+      datosApi.setAttribute("src", datos.faces[0].url[1][64]);
+
+      // Agregamos la tarjeta
+      contenedor.appendChild(tarjeta);
+      tarjeta.appendChild(datosApi);
+    });
+  } else {
+    const contenedor = document.getElementById("contenedor");
+    const mensajeError = document.createElement("div");
+
+    mensajeError.setAttribute("class", "alert alert-danger");
+    mensajeError.textContent = `Ha ocurrido un error con código ${request.status}`;
+
+    contenedor.appendChild(mensajeError);
+  }
+};
+
+request.send();
