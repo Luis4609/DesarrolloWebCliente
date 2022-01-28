9ae21a410376b1e467212f6b0b261f18383af2f4
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-01-28 17:01:09 +0100

    API imagenes Authors

--- Tema 5/0.3 Imagenes/Authors/main.js
+++ Tema 5/0.3 Imagenes/Authors/main.js
@@ -1,28 +1,65 @@
+var idNode = 0;
+
 function createNode(element) {
   var node = document.createElement(element);
+  node.idNode = idNode++;
   node.classList.add("node-img");
   node.setAttribute("tabindex", 1);
   node.addEventListener("click", selectImage);
   node.addEventListener("onmouseover", resalta);
   node.addEventListener("onmouseout", resalta);
-  node.addEventListener("keydown", function(event) {
-      // ENTER detectado
-      if (event.keyCode == 13 || event.keyCode == 9 && node.selected == true) {
-        // Código para la tecla ENTER
-        console.log("Oprimiste la tecla ENTER o TAB");
-        this.style.border = "5px solid red";
-        this.isSelected = true;
+  node.addEventListener("keydown", function (event) {
+    // ENTER detectado
+    if (event.keyCode == 13 || (event.keyCode == 9 && node.selected == true)) {
+      // Código para la tecla ENTER
+      console.log("Oprimiste la tecla ENTER o TAB");
+      this.style.border = "5px solid red";
+      this.isSelected = true;
+    }
+    // TAB detectado
+    if (event.keyCode == 9) {
+      // Código para la tecla TAB
+      console.log("Oprimiste la tecla TAB");
+      if (!this.isSelected) {
+        this.style.border = "5px solid green";
       }
-      // TAB detectado
-      if (event.keyCode == 9) {
-        // Código para la tecla TAB
-        console.log("Oprimiste la tecla TAB");
-        if(!this.isSelected){
-          this.style.border = "5px solid green";
-        }
-       
+    }
+    //Move with arrow keys
+    if (event.keyCode === 37 && node.selected == true) {
+      this.left = true;
+      this.style.border = "5px solid red";
+      if(this.idNode != 0){
+        this.previousSibling.style.border = "5px solid yellow";
       }
-    });
+    }
+    // only one key per event
+    if (event.keyCode === 38) {
+      this.up = true;
+      this.style.border = "5px solid red";
+      this
+    }
+    // so check exclusively
+    if (event.keyCode === 39) {
+      this.right = true;
+      this.style.border = "5px solid red";
+      if(this.idNode != 99){
+        this.nextSibling.style.border = "5px solid yellow";
+      }
+    }
+    if (event.keyCode === 40) {
+      this.down = true;
+      this.style.border = "5px solid red";
+    }
+  });
+  node.addEventListener("onkeyup", function (event) {
+    var kc = e.keyCode;
+    e.preventDefault();
+
+    if (kc === 37) Keys.left = false;
+    else if (kc === 38) Keys.up = false;
+    else if (kc === 39) Keys.right = false;
+    else if (kc === 40) Keys.down = false;
+  });
   return node;
 }
 
@@ -38,16 +75,15 @@ function selectImage() {
 
 function resalta(elEvento) {
   var evento = elEvento || window.event;
-  switch(evento.type) {
-    case 'mouseover':
-      this.style.borderColor = 'black';
+  switch (evento.type) {
+    case "mouseover":
+      this.style.borderColor = "black";
       break;
-    case 'mouseout':
-      this.style.borderColor = 'silver';
+    case "mouseout":
+      this.style.borderColor = "silver";
       break;
   }
 }
-
 
 const container = document.getElementById("authors");
 const url = "https://randomuser.me/api/?results=100";
