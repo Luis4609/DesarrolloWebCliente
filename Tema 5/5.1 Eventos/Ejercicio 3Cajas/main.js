f4720a1e8fba741d89978b1f573ad7052a2a9043
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-02-07 12:11:58 +0100

    Ejercicio forms 3 cajas

--- Tema 5/5.1 Eventos/Ejercicio 3Cajas/main.js
+++ Tema 5/5.1 Eventos/Ejercicio 3Cajas/main.js
@@ -6,21 +6,14 @@ const formParent = document.getElementById("form");
 // RegEx email
 // /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
 
-const expClave = /\W{6}/;
-const expEmail = /[@]/;
+const expClave = /\w{6}/;
+const expEmail = /@/;
 const expCodigoPostal = /\d{5}/;
 
 //HTML elements
 const claveElement = document.getElementById("clave");
 const emailElement = document.getElementById("email1");
 const codigoPostalElement = document.getElementById("codigoPostal");
-//Forms
-const clave = document.forms["myForm"]["clave"].value;
-const email = document.forms["myForm"]["email1"].value;
-const codigoPostal = document.forms["myForm"]["codigoPostal"].value;
-
-var valueEmail = emailElement.value;
-console.log(valueEmail);
 
 //Icons
 //Checked
@@ -29,7 +22,7 @@ console.log(valueEmail);
 /* <i class="fas fa-times"></i> */
 
 claveElement.addEventListener("change", () => {
-  if (expClave.test(clave)) {
+  if (expClave.test(claveElement.value)) {
     var iconCheck = document.createElement("i");
     iconCheck.innerHTML = '<i class="fas fa-check"></i>';
     append(formParent, iconCheck);
@@ -39,11 +32,10 @@ claveElement.addEventListener("change", () => {
     iconUnchecked.innerHTML = '<i class="fas fa-times"></i>';
     append(formParent, iconUnchecked);
     console.log("Mala clave");
-    console.log(valueEmail);
   }
 });
 emailElement.addEventListener("change", () => {
-  if (expEmail.test(email)) {
+  if (expEmail.test(emailElement.value)) {
     var iconCheck = document.createElement("i");
     iconCheck.innerHTML = '<i class="fas fa-check"></i>';
     append(formParent, iconCheck);
@@ -54,7 +46,7 @@ emailElement.addEventListener("change", () => {
   }
 });
 codigoPostalElement.addEventListener("change", () => {
-  if (expCodigoPostal.test(codigoPostal)) {
+  if (expCodigoPostal.test(codigoPostalElement.value)) {
     var iconCheck = document.createElement("i");
     iconCheck.innerHTML = '<i class="fas fa-check"></i>';
     append(formParent, iconCheck);
